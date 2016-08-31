8184ed818eca174111f7c062f1f7b266088ec907
Tom Dale <tom@tomdale.net>
2016-08-31 18:44:55 -0400

    Cleanup integration test capitalization

--- test/integration-test.ts
+++ test/integration-test.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { Model, Controller } from "../src";
 import * as test from "./helpers/application";
 
-describe("route dispatching", function() {
+describe("Integration: Route Dispatching", function() {
 
   describe("responses", function() {
 
