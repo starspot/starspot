a97760ae57c56cc894a4d5b816d04629c1221618
Tom Dale <tom@tomdale.net>
2016-11-03 14:32:28 -0400

    HTML with CSS

--- packages/starspot-docs/scripts/build-book.sh
+++ packages/starspot-docs/scripts/build-book.sh
@@ -4,33 +4,40 @@ rm -rf dist
 mkdir dist
 
 find_source_files() {
+  echo "\033[34mBuilding ${1}\033[0m"
   SOURCE_FILES=`find ./$1 -iname "*.md"`
   DIST="dist/${1}"
   BOOK_NAME=$1
   mkdir $DIST
 }
 
+echo_file() {
+  printf '  %-14s-> \e[36m%s\e[0m\n' "Building ${1}" "${2}"
+}
+
 build_pdf() {
-  pandoc --standalone --smart -o "${DIST}/${BOOK_NAME}.pdf" $SOURCE_FILES
+  local pdf="${DIST}/${BOOK_NAME}.pdf"
+  echo_file "pdf" "${pdf}"
+  pandoc --standalone --smart --output="${pdf}" $SOURCE_FILES
 }
 
 build_html() {
-  pandoc --standalone -o "${DIST}/${BOOK_NAME}.html" $SOURCE_FILES
+  local html="${DIST}/${BOOK_NAME}.html"
+  echo_file "html" "${html}"
+  pandoc --standalone --toc --css="../../assets/book.css" --output="${html}" $SOURCE_FILES
 }
 
 build_epub() {
-  pandoc --standalone -o "${DIST}/${BOOK_NAME}.epub" "${BOOK_NAME}/00-title.txt" $SOURCE_FILES
+  local epub="${DIST}/${BOOK_NAME}.epub"
+  echo_file "epub" "${epub}"
+  pandoc --standalone --output="${epub}" "${BOOK_NAME}/00-title.txt" $SOURCE_FILES
 }
 
 for book in "operator-guide" "mechanics-guide"
 do
   find_source_files $book
-  build_pdf
   build_html
   build_epub
+  build_pdf
+  echo
 done
-
-# pandoc --standalone --smart -o ./dist/starspot-operators-guide.pdf $SOURCE_FILES
-# echo $SOURCE_FILES
-# find ./book \( -iname "*.md" -or -iname "*.txt" \) -exec pandoc --standalone --smart -o ./dist/starspot-operators-guide.pdf {} \+
-
