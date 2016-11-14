07f9d959e856a6bf28ee8dda26ba3bf780b4009b
Lindsey Smith <lindsey@kickstarter.com>
2016-11-14 13:50:27 -0500

    Add a simple reflector allowing POJO models to be serialized

--- packages/starspot-core/src/index.ts
+++ packages/starspot-core/src/index.ts
@@ -2,7 +2,7 @@ export { default as Resolver } from "./resolver"
 export { default as Container } from "./container"
 export { default as Controller } from "./controller";
 export { default as Model } from "./model";
-export { default as Reflector } from "./reflector";
+export { default as Reflector, StaticReflector } from "./reflector";
 export { default as Inquirer } from "./inquirer";
 export { default as Application } from "./application";
 export { default as Router } from "./router";
